923c086a897ca34b9c4b48c4ed522c0a6acde1f3
Ilza Medeiros <ilza.medeiros@liferay.com>
2024-08-23 11:33:01 -0300

    LPD-34346 - Add js imports

--- modules/apps/portal-settings/portal-settings-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/portal-settings/portal-settings-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,35 +3,5 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {CountryRegionDynamicSelect} from '@liferay/address-web';
-
-function main({namespace}) {
-	AUI().use('liferay-auto-fields', () => {
-		new Liferay.AutoFields({
-			contentBox: `#${namespace}addresses`,
-			fieldIndexes: `${namespace}addressesIndexes`,
-			namespace,
-			on: {
-				clone(event) {
-					const guid = event.guid;
-					const row = event.row;
-
-					const dynamicSelects = row.one(
-						'select[data-componentType=dynamic_select]'
-					);
-
-					if (dynamicSelects) {
-						dynamicSelects.detach('change');
-					}
-
-					CountryRegionDynamicSelect.default({
-						countrySelect: `${namespace}addressCountryId${guid}`,
-						regionSelect: `${namespace}addressRegionId${guid}`,
-					});
-				},
-			},
-		}).render();
-	});
-}
-
-export {CountryRegionDynamicSelect, main};
+export {CountryRegionDynamicSelect, main} from './main';
+export {default as addJsClientExtension} from './admin-client-extension/AddJsClientExtension';
