7ea645827241cc5c11839790679ae6a3dc8680b7
Anderson Luiz <anderson.luiz@liferay.com>
2025-02-26 16:56:06 -0300

    LPD-49787 Delete menu access configuration asynchronously when a role is deleted

--- modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/model/listener/RoleModelListener.java
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/model/listener/RoleModelListener.java
@@ -14,7 +14,12 @@ import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.site.configuration.manager.MenuAccessConfigurationManager;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -25,21 +30,42 @@ public class RoleModelListener extends BaseModelListener<Role> {
 
 	@Override
 	public void onAfterRemove(Role role) throws ModelListenerException {
+		if (_executorService == null) {
+			return;
+		}
+
 		if ((role.getType() == RoleConstants.TYPE_REGULAR) ||
 			(role.getType() == RoleConstants.TYPE_SITE)) {
 
-			try {
-				_menuAccessConfigurationManager.deleteRoleAccessToControlMenu(
-					role);
-			}
-			catch (Exception exception) {
-				_log.error(exception);
-			}
+			_executorService.execute(
+				() -> {
+					try {
+						_menuAccessConfigurationManager.
+							deleteRoleAccessToControlMenu(role);
+					}
+					catch (Exception exception) {
+						_log.error(exception);
+					}
+				});
+		}
+	}
+
+	@Activate
+	protected void activate() {
+		_executorService = Executors.newFixedThreadPool(10);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_executorService != null) {
+			_executorService.shutdown();
 		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		RoleModelListener.class);
+
+	private ExecutorService _executorService;
 
 	@Reference
 	private MenuAccessConfigurationManager _menuAccessConfigurationManager;
